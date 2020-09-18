50ed332b0963a55def84150631e54ae464e8cdcd
Heberton.Nascimento <heberton.cdd@gmail.com>
2020-09-17 21:11:29 -0300

    Implementação com as principais funcionalidade do GIT

---
+++ Comandos _Git.sql
@@ -0,0 +1,14 @@
+-- Comando iniciais para o GIT
+
+- git init
+- git clone /caminho/para/o/repositório
+- git clone usuário@servidor:/caminho/para/o/repositório
+- git add <arquivo>
+- git commit -m "comentários das alterações"
+- git push origin master
+- git remote add origin <servidor>
+
+- git checkout -b funcionalidade_x (criar)
+- git checkout master
+- git branch -d funcionalidade_x   (remover)
+- git push origin <funcionalidade_x> (enviar para o repositorio)
